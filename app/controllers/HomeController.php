acf5323a5cf344f8d55bfc9837aeaba892adef3e
Bilal-EZ-ZAIM <bilalzaim499@gmail.com>
2023-12-30 15:24:30 +0100

    je terminte le page de home et je afiche les evilment

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,8 +6,14 @@ class Home extends Controller
 
     public function index()
     {
+        $eventes = new Eve;
+        $toutEventes = $eventes->findAll();
+        if(count($toutEventes) > 0){
+            $this->view('home', $toutEventes);
+        }else{
+            $this->view('home');
+        }
         
-        $this->view('home');
     }
 
 }
